1377d11fe3b72d2812f8a783d000a301def978c3
Klaudia <klaud.subs@gmail.com>
2025-07-28 23:11:59 +0200

    Setup

---
+++ src/main/java/com/sawaklaudia/gitHubRepositoriesViewer/GitHubRepositoriesViewerApplication.java
@@ -0,0 +1,13 @@
+package com.sawaklaudia.gitHubRepositoriesViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepositoriesViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubRepositoriesViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sawaklaudia/gitHubRepositoriesViewer/GitHubRepositoriesViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawaklaudia.gitHubRepositoriesViewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepositoriesViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
