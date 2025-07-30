5e58d6d69de8d8656f7f6463d80aa262c77b82e7
Klaudia <klaud.subs@gmail.com>
2025-07-30 22:05:08 +0200

    Create a method to get all user repositories.

--- src/main/java/com/sawaklaudia/gitHubRepositoriesViewer/service/GitHubRepositoriesService.java
+++ src/main/java/com/sawaklaudia/gitHubRepositoriesViewer/service/GitHubRepositoriesService.java
@@ -1,11 +1,31 @@
 package com.sawaklaudia.gitHubRepositoriesViewer.service;
 
+import com.sawaklaudia.gitHubRepositoriesViewer.retrofit.GitHubApi;
+import com.sawaklaudia.gitHubRepositoriesViewer.model.Repo;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GitHubRepositoriesService {
 
+    private final GitHubApi gitHubApi;
+
+    public GitHubRepositoriesService(GitHubApi gitHubApi) {
+        this.gitHubApi = gitHubApi;
+    }
+
     public String getRepositories(String username) {
-        return "";
+        try {
+            List<Repo> repos = gitHubApi.listRepos(username).execute().body();
+
+            return repos.stream()
+                    .map(repo -> repo.name())
+                    .collect(Collectors.joining(", "));
+        } catch (IOException e) {
+            throw new RuntimeException("Error downloading user repositories.");
+        }
     }
 }
